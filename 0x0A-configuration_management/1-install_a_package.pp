1d6c0caec1d94a42c8d8f63f479b5c17eef27f36
Fatima20027 <fatima.hailou@icloud.com>
2024-06-28 12:35:03 +0100

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,7 @@
-# 1-install_a_package.pp
+# install flask
 
-# Install python3-pip if not already installed
-package { 'python3-pip':
-  ensure => installed,
-}
 
-# Install Flask version 2.1.0 using pip3
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip'],  # Ensure python3-pip is installed beforehand
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0 Werkzeug==2.1.1',
+  unless  => '/usr/bin/pip3 show flask | grep Version | grep -q 2.1.0',
 }
